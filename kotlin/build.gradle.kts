027644e10f18a2e4110fdbf34a69896a3b6a9cfa
Takanori Ugai <ugai.takanori@gmail.com>
2026-04-04 20:34:26 +0900

    add dokka plugin

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -7,6 +7,8 @@ plugins {
     kotlin("plugin.serialization") version "2.3.10"
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
     id("io.gitlab.arturbosch.detekt") version "1.23.8"
+    id("org.jetbrains.dokka") version "2.2.0"
+    id("org.jetbrains.dokka-javadoc") version "2.2.0"
 }
 
 detekt {
