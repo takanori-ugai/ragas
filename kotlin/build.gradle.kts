45b242a071861caad62542169752837560c2ea32
Takanori Ugai <ugai.takanori@gmail.com>
2026-08-23 17:09:00 +0900

    Update build.gradle.kts

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     implementation("dev.langchain4j:langchain4j-open-ai:1.19.0")
     implementation("dev.langchain4j:langchain4j-azure-open-ai:1.19.0")
     implementation("dev.langchain4j:langchain4j-ollama:1.19.0")
-    implementation("dev.langchain4j:langchain4j-google-genai:1.19.0-beta28")
+    implementation("dev.langchain4j:langchain4j-google-genai:1.19.0-beta29")
     implementation("dev.langchain4j:langchain4j-google-ai-gemini:1.19.0")
     implementation("dev.langchain4j:langchain4j-community-neo4j:1.19.0-beta29")
     implementation("com.langchain.smith:langsmith-java:0.1.0-beta.22")
